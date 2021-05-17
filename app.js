3c76f0d8f4d19f7d9f81472e9e973d9680d930a2
kmayer <kylemayer@x.local>
2021-05-17 14:06:33 -0700

    imported raw pokemon data and grabbed DOM elements

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
+import pokemonData from './data/pokemon.js';
+
+
+
 // import functions and grab DOM elements
+const poke1 = document.querySelector('#pokemon1');
+const poke2 = document.querySelector('#pokemon2');
+const poke3 = document.querySelector('#pokemon3');
+
+const img1= document.querySelector('#image1');
+const img2 = document.querySelector('#image2');
+const img3 = document.querySelector('#image3');
+
+const button = document.querySelector('#button');
+
 
 // initialize state
 
@@ -6,3 +20,5 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+  
