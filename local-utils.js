cb6753a45263bbad602fa9c8cf723085376cff38
kmayer <kylemayer@x.local>
2021-05-17 16:42:00 -0700

    eslint errors

--- local-utils.js
+++ local-utils.js
@@ -3,26 +3,26 @@ import { findById } from './utils.js'; // findById will loop through the pokemon
 
 const POKEDEX = 'POKEDEX';
 
-export function getPokedex () {
+export function getPokedex() {
     const stringyPokedex = localStorage.getItem(POKEDEX); // get pokedex from local storage - currently a string
     if (!stringyPokedex) return []; // if no local storage pokedex available - return empty array 
     const pokedex = JSON.parse(stringyPokedex); // set pokedex to parsed pokedex from local storage
     return pokedex; 
 }
 
-export function setPokedex (newPokedex) {
+export function setPokedex(newPokedex) {
     const stringyPokedex = JSON.stringify(newPokedex); // stringifying the new Pokedex and setting to stringyPokedex
     localStorage.setItem(POKEDEX, stringyPokedex); // setting POKEDEX and stringified new Pokedex into local storage
 }
 
-export function catchPoke (checkedPokemon) { // 
+export function catchPoke(checkedPokemon) { // 
     const pokedex = getPokedex(); // setting pokedex equal to the pokedex that the getPokedex function returns
     const samePokemon = findById(pokedex, checkedPokemon); // setting samePokemon equal to the 
     samePokemon.caught++; //
     setPokedex(pokedex); // saves/"sets" new pokedex values into local storage
 }
 
-export function encounterPoke (checkedPokemon) {
+export function encounterPoke(checkedPokemon) {
     const pokedex = getPokedex();
 
     const samePokemon = findById(pokedex, checkedPokemon); //
